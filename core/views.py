ef7c8cb7e9d5b1efe18cff314ddb9d45745a8a51
angcastillo1897 <angcastillo18@gmail.com>
2025-04-10 01:16:24 -0500

    erased views.py from core

--- core/views.py
+++
@@ -1,8 +0,0 @@
-# from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-
-
-@api_view(["GET"])
-def getTest(request):
-    return Response({"message": "Hello, World!"})
